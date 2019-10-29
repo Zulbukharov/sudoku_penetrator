1272037eee1bdceb553b73f4b32632651e917f50
Abylaikhan Zulbukharov <l1pt0n1905@gmail.com>
2019-10-29 15:00:39 +0600

    [add]

---
+++ corrector.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+COUNTER=1
+cat hello
+echo
+while read p; do
+	OUT=$(./a.out $p | tr  '\n' ' ' | tr '$' ' ' | sed -e "s/ //g")
+	if grep -q $OUT "correct.out"; then
+		echo -e "[`whoami`]${GREEN}[PASSED]${NC}[${COUNTER}]"
+	else
+		echo -e "[`whoami`]${RED}[FAILED]${NC}[${COUNTER}][${OUT}]"
+	fi
+	COUNTER=$((COUNTER + 1))
+done < easy_tests
